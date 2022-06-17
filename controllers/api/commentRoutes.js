51f1688b41c6c8aed5b806ab710d1d5521589dcb
choyoonme <yoonme@gmail.com>
2022-06-16 21:30:17 -0400

    fix bug

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -14,9 +14,9 @@ router.get("/", (req, res) => {
 router.post("/", withAuth, (req, res) => {
   if (req.session) {
     Comment.create({
-      comment_text: req.body.comment_text,
-      post_id: req.body.post_id,
       user_id: req.session.user_id,
+      post_id: req.body.post_id,
+      comment_text: req.body.comment_text,
     })
       .then((dbCommentData) => res.json(dbCommentData))
       .catch((err) => {
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,36 +2,34 @@ const { User } = require("../models");
 
 const userData = [
   {
-    name: "martin_bour",
-    email: "martin_b@gmail.com",
-    password: "p@ssword1",
+    name: "taylor",
+    email: "taylor@gmail.com",
+    password: "password1",
   },
   {
-    name: "matt_b",
-    email: "mathew_b@gmail.com",
-    password: "p@ssword2",
+    name: "seymour",
+    email: "seymour@gmail.com",
+    password: "password2",
   },
   {
-    name: "shaun_c",
-
-    email: "shaun_c@gmail.com",
-    password: "p@ssword3",
+    name: "nancy",
+    email: "nancy@gmail.com",
+    password: "password3",
   },
   {
-    name: "lee_n",
-
-    email: "lee_n@gmail.com",
-    password: "p@ssword4",
+    name: "phil",
+    email: "phil@gmail.com",
+    password: "password4",
   },
   {
-    name: "priya_r",
-    email: "priya_r@gmail.com",
-    password: "p@ssword5",
+    name: "barry",
+    email: "barry@gmail.com",
+    password: "password5",
   },
   {
-    name: "pooja",
-    email: "pooja@gmail.com",
-    password: "p@ssword6",
+    name: "paula",
+    email: "paula@gmail.com",
+    password: "password6",
   },
 ];
 
