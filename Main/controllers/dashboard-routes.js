b98b645397da4bce73dbdb4f733abfe412f758ac
choyoonme <yoonme@gmail.com>
2022-07-18 16:33:59 -0400

    change wording

--- Main/controllers/dashboard-routes.js
+++ Main/controllers/dashboard-routes.js
@@ -1,82 +1,94 @@
-const router = require('express').Router();
-const { Post, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all Posts and JOIN with user data
-    const PostData = await Post.findAll({
-      include: [
-        {
+router.get("/", withAuth, async (req, res) => {
+  await Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "content"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id"],
+        include: {
           model: User,
-          attributes: ['name'],
+          attributes: ["username"],
         },
-      ],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((PostData) => {
+      const profile = PostData.map((data) =>
+        data.get({
+          plain: true,
+        })
+      );
+      res.render("dashboard", {
+        profile,
+        loggedIn: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    // Serialize data so the template can read it
-    const post = PostData.map((Post) => Post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      ...post,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.get('/Post/:id', async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
+router.get("/edit/:id", withAuth, async (req, res) => {
+  await Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
           model: User,
-          attributes: ['name'],
+          attributes: ["username"],
         },
-      ],
-    });
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((PostData) => {
+      if (!PostData) {
+        res.status(404).json({
+          message: "No post found with this id",
+        });
+        return;
+      }
 
-    const post = postData.get({ plain: true });
+      const post = PostData.get({
+        plain: true,
+      });
 
-    res.render('post', {
-      ...post,
-      logged_in: req.session.logged_in,
+      res.render("edit-post", {
+        post,
+        loggedIn: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
+router.get("/new", (req, res) => {
+  res.render("add-post", {
+    loggedIn: true,
+  });
 });
 
 module.exports = router;
